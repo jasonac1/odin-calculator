5389a332fe4033225d1d6a97ae4d409195b779e2
jasonac <143886081+jasonac1@users.noreply.github.com>
2026-01-19 23:09:09 +0700

    Add click to display digit logic

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let number1 = 0;
 let operator = ""; 
-let number = 0; 
+let number2 = 0; 
+
+const display = document.querySelector(".box-display p");
+const buttonContainer = document.querySelector(".container-buttons");
 
 function add(a, b) {
     return a + b;
@@ -42,7 +45,21 @@ function operate(a, operator, b) {
     return result;
 }
 
-// console.log(operate(6, "+", 7));
-// console.log(operate(6, "-", 7));
-// console.log(operate(6, "*", 7));
-// console.log(operate(6, "/", 7));
+function updateNumber(e) {
+	let newDigit = +(e.target.textContent); 
+	if (number1 == null) number1 = newDigit; // starting point      
+	else {
+        number1 *= 10 
+        number1 += newDigit; // display / number already has one digit or more
+    }
+} 
+
+function updateDisplay(e)  {
+    display.textContent = number1;
+}
+
+buttonContainer.addEventListener("click", (e) => {
+    updateNumber(e);
+    updateDisplay(e);
+})
+	
