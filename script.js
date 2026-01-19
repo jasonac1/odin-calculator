fbb038f79d2722dfb688f325d457798d1a3067c1
jasonac <143886081+jasonac1@users.noreply.github.com>
2026-01-19 17:47:19 +0700

    Create arithmetic functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// console.log(add(6, 7));
+// console.log(subtract(6, 7));
+// console.log(multiply(6, 7));
+// console.log(divide(6, 7));
