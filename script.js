44857d98b5a01d0e970b1b8ed8b4382c80b8007b
jasonac <143886081+jasonac1@users.noreply.github.com>
2026-01-19 18:17:30 +0700

    Add operate function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let number1 = 0;
 let operator = ""; 
-let number2 = 0; 
+let number = 0; 
 
 function add(a, b) {
     return a + b;
@@ -18,7 +18,31 @@ function divide(a, b) {
     return a / b;
 }
 
-// console.log(add(6, 7));
-// console.log(subtract(6, 7));
-// console.log(multiply(6, 7));
-// console.log(divide(6, 7));
+function operate(a, operator, b) {
+    let result = 0;
+
+    switch (operator) {
+        case "+":
+            result = add(a, b);
+            break;
+    
+        case "-":
+            result = subtract(a, b);
+            break;
+    
+        case "*":
+            result = multiply(a, b);
+            break;
+    
+        case "/":
+            result = divide(a, b);
+            break;
+    }
+
+    return result;
+}
+
+// console.log(operate(6, "+", 7));
+// console.log(operate(6, "-", 7));
+// console.log(operate(6, "*", 7));
+// console.log(operate(6, "/", 7));
